06d4f8f6a4ecf91a061f5883d719aaa312d960ab
Anderson S. Silva <andsoares2001@gmail.com>
2020-11-24 19:39:47 -0300

    Create contato.php

---
+++ contato.php
@@ -0,0 +1,16 @@
+<div id="map"></div>
+<div class="contato-container">
+	<div class="center">
+		<form>
+			<input type="text" name="Nome" placeholder="Nome...">
+			<div></div>
+			<input type="text" name="E-mail" placeholder="E-mail...">
+			<div></div>
+			<input type="text" name="Telefone" placeholder="Telefone...">
+			<div></div>
+			<textarea name="mensagem" placeholder="Sua mensagem..."></textarea>
+			<div></div>
+			<input type="submit" name="acao" value="Enviar">
+		</form>
+	</div>
+</div>
