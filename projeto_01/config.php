6a91eed55b6e83b054454f78ed958d50b2b235c9
Anderson S. Silva <andsoares2001@gmail.com>
2020-12-01 15:02:55 -0300

    Update config.php

--- projeto_01/config.php
+++ projeto_01/config.php
@@ -1,4 +1,13 @@
 <?php
+	$autoload = function($class){
+		if ($class == 'Email') {
+			include('classes/phpmailer/PHPMailerAutoload')
+		}
+		include('classes/'.$class.'.php');
+	};
+
+	spl_autoload_register($autoload)
+
 	define('INCLUDE_PATH', 'http://localhost/projeto_01/');
 ?>
 
